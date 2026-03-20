61296da4b0602e701c4b329df1dffe3008c97fe0
Rishik260123 <rishikbhardwaj0708@gmail.com>
2026-03-20 14:20:33 +0530

    final working backend

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,28 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// DB Pool
+
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'StudentResultSystem',
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-})
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   ssl: {
+//     minVersion: 'TLSv1.2',
+//     rejectUnauthorized: false
+//   }
+// });
 
 // Test DB connection on startup
 pool.getConnection()
